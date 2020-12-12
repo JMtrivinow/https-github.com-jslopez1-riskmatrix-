9624af5fbc514dadb9c4a840943a7fd2565e7373
jhonlopez <jhon.lopez@cyxtera.com>
2020-12-11 20:52:06 -0500

    add initial files

---
+++ src/main/java/com/bancofuturo/riskmatrix/RiskmatrixApplication.java
@@ -0,0 +1,13 @@
+package com.bancofuturo.riskmatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskmatrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskmatrixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bancofuturo/riskmatrix/RiskmatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancofuturo.riskmatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskmatrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
